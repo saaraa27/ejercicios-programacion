1331de8ecf7ee6c0562b60325079b351612672c8
Sara <sara.saezdecortazar.52977@ikasle.egibide.org>
2025-12-11 12:23:12 +0100

    segundo ejercicio 5

--- ejercicio3.2/src/Main.java
+++ ejercicio3.2/src/Main.java
@@ -1,15 +1,96 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    final private static Scanner sc = new Scanner(System.in);
+    private static Queue<String > lista = new ArrayDeque<>();
+    private static Queue<LocalDate> lista2 = new ArrayDeque<>();
+    private static LocalDate fecha2;
+
+    public static void main(String[] args) {
+        try{
+        fDatos();
+        fSolicitarFecha();
+        fEliminarProductos();
+        fMostar();
+
+    }catch (Exception e){
+        System.out.println("Ha ocurrido un error: " + e.getMessage());
         }
     }
+
+    public static void fDatos () {
+        String respuesta;
+        do {
+            try {
+                nombre();
+            }catch (NullPointerException e){
+                System.out.println("Error al introducir el nombre del producto");
+            }
+
+            try{
+            fecha();
+            }catch (DateTimeParseException e){
+                System.out.println("Error al introducir la fecha del producto:");
+            }
+
+            do{
+                System.out.println("Quiere introducir otro producto? (s/n)");
+                respuesta = sc.nextLine();
+            }while (!respuesta.equals("s") && !respuesta.equals("n"));
+
+        }while (respuesta.equals("s"));
+    }
+
+    public static void fSolicitarFecha (){
+        try {
+            System.out.println("Introduce la fecha a partir de la cual quiere eliminar los productos");
+            String fechaSolicitada = sc.nextLine();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            fecha2 = LocalDate.parse(fechaSolicitada, formatter);
+        }catch (DateTimeParseException e){
+            System.out.println("Error al introducir la fecha:");
+        }
+    }
+
+    public static void fEliminarProductos (){
+        while (!lista.isEmpty() && !lista2.isEmpty()){
+            LocalDate fechaProducto = lista2.peek();
+            if (fechaProducto.isBefore(fecha2) || fechaProducto.isEqual(fecha2)){
+                lista.poll();
+                lista2.poll();
+            } else {
+                break;
+            }
+        }
+    }
+
+    public static void fMostar(){
+        for (int i = 0; i<lista.size(); i++){
+            System.out.println("producto " + lista.peek() + " con fecha " + lista2.peek());
+        }
+    }
+
+
+
+
+    public static void nombre(){
+        System.out.print("Introduce el nombre del producto");
+        String nombreProducto = sc.nextLine();
+        lista.offer(nombreProducto);
+    }
+
+    public static void fecha(){
+        System.out.println("Introduce la fecha de caducidad del producto");
+        String fechaCaducidad = sc.nextLine();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate fecha = LocalDate.parse(fechaCaducidad, formatter);
+        lista2.offer(fecha);
+    }
+
 }
